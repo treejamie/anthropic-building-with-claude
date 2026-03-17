567ef6126470434d64ef63dee63ba6955822b910
treejamie <me@treejamie.com>
2026-03-17 08:59:39 +0000

    Added types into the basic fn library

--- fn.py
+++ fn.py
@@ -1,23 +1,34 @@
+
 from dotenv import load_dotenv
 from anthropic import Anthropic
+from anthropic.types import MessageParam, TextBlock
 
 
 load_dotenv()
 client = Anthropic()
 
 
-def add_user_message(messages, content):
-    user_message = {"role": "user", "content": content}
+def add_user_message(messages: list[MessageParam], content: str) -> None:
+    """Peasant context simulation"""
+    user_message: MessageParam = {"role": "user", "content": content}
     messages.append(user_message)
 
 
-def add_assistant_message(messages, content):
-    assistant_message = {"role": "assistant", "content": content}
+def add_assistant_message(messages: list[MessageParam], content: str) -> None:
+    """Peasant application management"""
+    assistant_message :MessageParam = {"role": "assistant", "content": content}
     messages.append(assistant_message)
 
 
-def chat(messages):
+def chat(messages: list[MessageParam]) -> str:
+    """Basic text response handling"""
     message = client.messages.create(
-        model="claude-haiku-4-5", max_tokens=1000, messages=messages
+        model="claude-haiku-4-5",
+        max_tokens=1000,
+        messages=messages
     )
-    return message.content[0].text
+
+    # make sure we're returning something capable of having text type
+    block = message.content[0]
+    assert isinstance(block, TextBlock)
+    return block.text
